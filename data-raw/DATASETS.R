90640304ac3229b5baf53954ac887b3a4ac754a9
terrance-indagwa <terranceindagw@gmail.com>
2021-10-01 10:20:47 +0300

    creating a data-raw file

--- data-raw/DATASETS.R
+++ data-raw/DATASETS.R
@@ -1,184 +1,174 @@
----
-title: "Kenya Power"
-author: "Dir.Terrance"
-date: "8/12/2021"
-output: html_document
----
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-```{r}
-library(tidyverse)
+library(dplyr)
 library(rvest)
 
-```
 
-```{r}
+
+
 raw <- read_html("https://en.wikipedia.org/wiki/List_of_power_stations_in_Kenya")
 
-raw <- raw %>% 
-  html_elements("table") 
-geothermal <- raw%>% 
-  html_table(.) %>% 
+raw <- raw %>%
+  html_elements("table")
+geothermal <- raw%>%
+  html_table(.) %>%
   .[[2]]
 
-hydroelectric <- raw %>% 
-  html_table() %>% 
+hydroelectric <- raw %>%
+  html_table() %>%
   .[[3]]
 
-fossils <- raw %>% 
-  html_table(.) %>% 
+fossils <- raw %>%
+  html_table(.) %>%
   .[[4]]
 
-wind <- raw %>% 
-  html_table(.) %>% 
+wind <- raw %>%
+  html_table(.) %>%
   .[[5]]
 
-solar <- raw %>% 
-  html_table(.) %>% 
+solar <- raw %>%
+  html_table(.) %>%
   .[[6]]
-```
+
 
 # Data Cleaning {#janitor}
 
 ## Geothermal
-```{r jan_geo}
+
 #changing colnames to lower case
 colnames(geothermal) <- str_to_lower(colnames(geothermal))
 
 #just selecting the first coordinates of the power plants.
-geothermal$location <- geothermal$location %>% 
- str_split_fixed( "/", n=2) %>% 
-  .[nrow(geothermal), 1] 
+geothermal$location <- geothermal$location %>%
+  str_split_fixed( "/", n=2) %>%
+  .[nrow(geothermal), 1]
 
 #removing cells index numbers that got pooled by mistake
 geothermal <- geothermal %>%
   mutate(
-  station=str_remove_all(station,"\\[.\\]"),
-  `capacity (mw)`=str_remove_all(`capacity (mw)`, "\\[.\\]"),
-  notes=str_remove_all(notes, "\\[.\\]"),
-  category=rep("geothermal", nrow(geothermal)))
+    station=str_remove_all(station,"\\[.\\]"),
+    `capacity (mw)`=str_remove_all(`capacity (mw)`, "\\[.\\]"),
+    notes=str_remove_all(notes, "\\[.\\]"),
+    category=rep("geothermal", nrow(geothermal)))
 
-write_csv(geothermal, "geo.csv")
-```
+  write.csv(geothermal, "data/geothermal.csv")
+
 
 
 
 ## Hydroelectric
 
-```{r jan_hydro}
+
 #changing colnames to lower case
 colnames(hydroelectric) <- str_to_lower(colnames(hydroelectric))
 
 #just selecting the first coordinates of the power plants.
-hydroelectric$location <- hydroelectric$location %>% 
- str_split_fixed( "/", n=2) %>% 
+hydroelectric$location <- hydroelectric$location %>%
+  str_split_fixed( "/", n=2) %>%
   .[1:15, 1]
 
-hydroelectric <- hydroelectric %>% 
+hydroelectric <- hydroelectric %>%
   mutate(
     station=str_remove_all(station, "\\[..\\]"),
     location=str_remove_all(location, "\\[..\\]"),
     type=str_remove_all(type, "\\[..\\]"),
     `capacity (mw)`=str_remove_all(`capacity (mw)`, "\\[..\\]"),
-   commissioned=str_remove_all(commissioned, "\\[..\\]"),
-   category=rep("hydroelectric", nrow(hydroelectric))
+    commissioned=str_remove_all(commissioned, "\\[..\\]"),
+    category=rep("hydroelectric", nrow(hydroelectric))
   )
 
-write_csv(hydroelectric, "hydroelectric.csv")
-```
+  write.csv(hydroelectric, "data/hydroelectric.csv")
+
 
 ## Fossils
 
-```{r jan_fossils}
+
 #changing colnames to lower case
 colnames(fossils) <- str_to_lower(colnames(fossils))
 
 #just selecting the first coordinates of the power plants.
-fossils$location <- fossils$location %>% 
- str_split_fixed( "/", n=2) %>% 
+fossils$location <- fossils$location %>%
+  str_split_fixed( "/", n=2) %>%
   .[1:10, 1]
 
-fossils <- fossils %>% 
-   mutate(
+fossils <- fossils %>%
+  mutate(
     station=str_remove_all(station, "\\[..\\]"),
     location=str_remove_all(location, "\\[..\\]"),
     type=str_remove_all(type, "\\[..\\]"),
     `capacity (mw)`=str_remove_all(`capacity (mw)`, "\\[..\\]"),
-   commissioned=str_remove_all(commissioned, "\\[..\\]"),
-   category=rep("fossils", nrow(fossils))
+    commissioned=str_remove_all(commissioned, "\\[..\\]"),
+    category=rep("fossils", nrow(fossils))
   )
-```
 
-Adding mini projects
-```{r jan_fossils2}
-mini <- tibble(
+
+#Adding mini projects
+
+mini <- data.frame(
   station= c("Dave Flower Farm", "Simbi Roses, Thika", "Oilvado Company ltd", "Pine Power ltd", "James Finlays ltd","Dagoreti Slaughter House"),
-  `capacity (mw)` =c(0.1, .055, 1.34, 1.15, 1.16, .23),
+  capacity=c(0.1, 0.055, 1.34, 1.15, 1.16, .23),
   commissioned=c("in development", "in development", "operational", "operational","operational", "operational"),
   type=rep("Biogas", 6)
 )
 
-fossils <- fossils %>% 
-  full_join(mini, by=c("station", "capacity (mw)", "commissioned", "type")) %>% 
+fossils <- fossils %>%
+  full_join(mini, by=c("station", "capacity (mw)", "commissioned", "type")) %>%
   mutate(category=rep("fossils", 16))
-write_csv(fossils, "fossils.csv")
-```
+  write.csv(fossils, "data/fossils.csv")
 
 
-```{r jan_fossils}
-```
+
+
 
 ## Solar
 
-```{r jan_solar}
+
 #changing colnames to lower case
 colnames(solar) <- str_to_lower(colnames(solar))
 
 #just selecting the first coordinates of the power plants.
-solar$location <- solar$coordinates %>% 
- str_split_fixed( "/", n=2) %>% 
+solar$location <- solar$coordinates %>%
+  str_split_fixed( "/", n=2) %>%
   .[1, 1]
 
-solar <- solar %>% 
-   mutate(
+solar <- solar %>%
+  mutate(
     station=str_remove_all(`solar power station`, "\\[..\\]"),
     location=str_remove_all(location, "\\[..\\]"),
     `capacity (mw)`=str_remove_all(`capacity (megawatts)`, "\\[..\\]"),
-   commissioned=str_remove_all(`year completed`, "\\[..\\]"),
-   category=rep("solar", nrow(solar))
-  ) %>% 
+    commissioned=str_remove_all(`year completed`, "\\[..\\]"),
+    category=rep("solar", nrow(solar))
+  ) %>%
   select(c(station, location, `capacity (mw)`, commissioned, category))
 
 
-write_csv(solar, "solar.csv")
-```
+  write.csv(solar, "data/solar.csv")
+
 
 ## Wind
 
-```{r jan_wind}
 
 
 #changing colnames to lower case
 colnames(wind) <- str_to_lower(colnames(wind))
 
 #just selecting the first coordinates of the power plants.
-wind$location <- wind$location %>% 
- str_split_fixed( "/", n=2) %>% 
+wind$location <- wind$location %>%
+  str_split_fixed( "/", n=2) %>%
   .[1:5, 1]
 
-wind <- wind %>% 
+wind <- wind %>%
   mutate(
     station=str_remove_all(station, "\\[..\\]"),
     location=str_remove_all(location, "\\[..\\]"),
     notes=str_remove_all(notes, "\\[..\\]"),
     `capacity (mw)`=str_remove_all(`capacity (mw)`, "\\[..\\]"),
-   commissioned=str_remove_all(commissioned, "\\[..\\]"),
-   category=rep("wind", nrow(wind))
+    commissioned=str_remove_all(commissioned, "\\[..\\]"),
+    category=rep("wind", nrow(wind))
   )
 
-write_csv(wind, "wind.csv")
-```
+  write.csv(wind, "data/wind.csv")
+
+
+
+
 
